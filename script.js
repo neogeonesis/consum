2decba82b79c8316b47df9125042ec2f328f8ab1
neogeonesis <108653528+neogeonesis@users.noreply.github.com>
2023-11-22 05:19:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function showPopup(imageSrc, description) {
-    document.getElementById('popupImage').src = imageSrc;
-    document.getElementById('popupDescription').innerText = description;
-    document.getElementById('popup').style.display = 'block';
-}
 
-function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-}
