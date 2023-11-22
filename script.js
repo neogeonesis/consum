7ecd7220a72ad7e12af174a6d535a10750acbff0
neogeonesis <108653528+neogeonesis@users.noreply.github.com>
2023-11-22 01:50:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,35 +6,50 @@ const popupImages = [
     // Agrega más imágenes según sea necesario
 ];
 
-// Función para mostrar un pop-up específico
-function showPopup(imageSrc, description) {
-    document.getElementById('popupImage').src = imageSrc;
-    document.getElementById('popupDescription').innerText = description;
-    document.getElementById('popup').style.display = 'block';
+// Número de pop-ups que se mostrarán
+const numberOfPopups = 20;
 
-    // Establece un tiempo para cerrar automáticamente el pop-up después de 5 segundos
+// Función para crear y mostrar un nuevo pop-up
+function createPopup() {
+    // Selecciona una imagen aleatoria
+    const randomImage = popupImages[Math.floor(Math.random() * popupImages.length)];
+
+    // Crea un nuevo elemento de pop-up
+    const newPopup = document.createElement('div');
+    newPopup.classList.add('popup');
+
+    // Crea una nueva imagen
+    const newImage = document.createElement('img');
+    newImage.src = randomImage;
+    newImage.alt = 'Popup Image';
+
+    // Agrega la imagen al pop-up
+    newPopup.appendChild(newImage);
+
+    // Posiciona el pop-up en una ubicación aleatoria
+    const randomX = Math.floor(Math.random() * (window.innerWidth - 200));
+    const randomY = Math.floor(Math.random() * (window.innerHeight - 200));
+    newPopup.style.left = `${randomX}px`;
+    newPopup.style.top = `${randomY}px`;
+
+    // Agrega el pop-up al cuerpo del documento
+    document.getElementById('popups-container').appendChild(newPopup);
+
+    // Establece un tiempo para eliminar el pop-up después de un tiempo (ajusta según sea necesario)
     setTimeout(() => {
-        closePopup();
+        document.getElementById('popups-container').removeChild(newPopup);
     }, 5000);
-}
-
-// Función para cerrar el pop-up
-function closePopup() {
-    document.getElementById('popup').style.display = 'none';
 }
 
 // Función para mostrar pop-ups continuamente
 function showPopups() {
-    // Establece un tiempo aleatorio para mostrar el próximo pop-up
-    const randomTime = Math.floor(Math.random() * (500 - 100 + 1) + 100);
-    setTimeout(() => {
-        // Selecciona una imagen aleatoria
-        const randomImage = popupImages[Math.floor(Math.random() * popupImages.length)];
-        // Muestra el pop-up con la imagen aleatoria y una descripción
-        showPopup(randomImage, 'Descripción aleatoria');
-        // Llama a la función nuevamente para mostrar el siguiente pop-up
-        showPopups();
-    }, randomTime);
+    // Muestra un número específico de pop-ups
+    for (let i = 0; i < numberOfPopups; i++) {
+        createPopup();
+    }
+
+    // Llama a la función nuevamente para mostrar el siguiente conjunto de pop-ups después de 5 segundos
+    setTimeout(showPopups, 5000);
 }
 
 // Inicia el ciclo de pop-ups automáticos
