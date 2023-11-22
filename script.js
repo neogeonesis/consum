fda2aa345a64d2883f91d46ee21993389969be19
neogeonesis <108653528+neogeonesis@users.noreply.github.com>
2023-11-22 01:58:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,46 +9,61 @@ const popupImages = [
 // Número de pop-ups que se mostrarán
 const numberOfPopups = 20;
 
-// Función para crear y mostrar un nuevo pop-up
+// Función para crear y mostrar un nuevo pop-up con movimiento
 function createPopup() {
-    // Selecciona una imagen aleatoria
     const randomImage = popupImages[Math.floor(Math.random() * popupImages.length)];
-
-    // Crea un nuevo elemento de pop-up
     const newPopup = document.createElement('div');
     newPopup.classList.add('popup');
 
-    // Crea una nueva imagen
     const newImage = document.createElement('img');
     newImage.src = randomImage;
     newImage.alt = 'Popup Image';
 
-    // Agrega la imagen al pop-up
     newPopup.appendChild(newImage);
-
-    // Posiciona el pop-up en una ubicación aleatoria
-    const randomX = Math.floor(Math.random() * (window.innerWidth - 200));
-    const randomY = Math.floor(Math.random() * (window.innerHeight - 200));
-    newPopup.style.left = `${randomX}px`;
-    newPopup.style.top = `${randomY}px`;
-
-    // Agrega el pop-up al cuerpo del documento
     document.getElementById('popups-container').appendChild(newPopup);
 
-    // Establece un tiempo para eliminar el pop-up después de un tiempo (ajusta según sea necesario)
+    // Inicia el movimiento del pop-up
+    movePopup(newPopup);
+}
+
+// Función para mover un pop-up con rebote
+function movePopup(popup) {
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() * window.innerHeight;
+    let speedX = (Math.random() - 0.5) * 5;
+    let speedY = (Math.random() - 0.5) * 5;
+
+    function animate() {
+        x += speedX;
+        y += speedY;
+
+        if (x + 200 > window.innerWidth || x < 0) {
+            speedX = -speedX;
+        }
+
+        if (y + 200 > window.innerHeight || y < 0) {
+            speedY = -speedY;
+        }
+
+        popup.style.left = `${x}px`;
+        popup.style.top = `${y}px`;
+
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+
     setTimeout(() => {
-        document.getElementById('popups-container').removeChild(newPopup);
+        document.getElementById('popups-container').removeChild(popup);
     }, 5000);
 }
 
 // Función para mostrar pop-ups continuamente
 function showPopups() {
-    // Muestra un número específico de pop-ups
     for (let i = 0; i < numberOfPopups; i++) {
         createPopup();
     }
 
-    // Llama a la función nuevamente para mostrar el siguiente conjunto de pop-ups después de 5 segundos
     setTimeout(showPopups, 5000);
 }
 
